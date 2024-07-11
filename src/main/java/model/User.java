f11d6212e31d8846f5c8bee10e79fe47b6d36bfa
Azhaar Mohamed <azhaar.mohamed20@gmail.com>
2024-07-11 08:51:22 +0200

    Create User Model

---
+++ src/main/java/model/User.java
@@ -0,0 +1,49 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Setter;
+
+@Data
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userid;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(length = 50, unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "hashed_password")
+    private String password;
+
+    private String role;
+
+    public User(){
+        super();
+    }
+
+    public User(String firstName, String lastName, String email, String password, String role) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+    public User(String firstName, String lastName, String email, String password) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        // this.role = role; Interface für Standard Rolle setzen.
+    }
+}
