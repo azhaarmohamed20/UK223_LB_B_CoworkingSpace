a5c8cb5c0484a009d5b16a9d6d337db2f16721d8
Azhaar Mohamed <azhaar.mohamed20@gmail.com>
2024-07-11 13:20:17 +0200

    Create UserService

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,30 @@
+package service;
+
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id){
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
+    // Update User als zusätzliche Implementierung
+}
